356d26db9596eb1f63b00d3274ff24ad9630e625
Marek Hewelt <22530856+lightflicker@users.noreply.github.com>
2019-06-23 00:49:10 +0100

    Brought the code to PEP8 standard with flake8

--- Sequencer/RainbowVail.py
+++ Sequencer/RainbowVail.py
@@ -9,25 +9,27 @@ import csv
 import serial
 import sys
 import time
+import curses
+from curses import wrapper
 
-#Initialisation of the dictionary list. The list will hold information about the step paramters
-
+# Initialisation of the dictionary list. The list will hold information
+# about the step paramters
 seq = []
 
 reader = csv.DictReader(open(sys.argv[1], 'rt', encoding="utf8"))
 for line in reader:
     seq.append(line)
 
-
-#Definition of Step 0
-# configure the serial connections (the parameters differs on the device you are connecting to)
+# Definition of Step 0
+# configure the serial connections (the parameters differs on the device
+# you are connecting to)
 ser = serial.Serial(
     port='/dev/ttyUSB0',
     baudrate=38400,
     timeout=10
 )
-#ser.isOpen()
-#time.sleep(2)
+# ser.isOpen()
+# time.sleep(2)
 out = ''
 
 print('Enter your commands below.\r\nInsert "exit" to leave the application.')
@@ -36,40 +38,56 @@ input = 1
 StepNo = 0
 Delimiter = ';'
 while 1:
-    #Collect the data if ready for collection
+    # Collect the data if ready for collection
     time.sleep(0.01)
     while ser.inWaiting() > 0:
         out = ser.readline()
-        print('Received: ' + out.decode('utf-8') + 'Lenght: ' + str(len(out)) + ' (bytes)')
+        print('Received: ' + out.decode('utf-8') +
+              'Lenght: ' + str(len(out)) + ' (bytes)')
 
-
-    #Load step paramters
+    # Load step paramters
     dataStr = "<" + str(seq[StepNo]['iTypeC'])
-    dataStr +=  Delimiter + str(seq[StepNo]['fCtr_A']) + Delimiter + str(seq[StepNo]['fCtr_B']) + Delimiter + str(seq[StepNo]['fCtr_C']) + Delimiter + str(seq[StepNo]['fCtr_D']) + Delimiter + str(seq[StepNo]['fCtr_E'])
-    dataStr +=  Delimiter + str(seq[StepNo]['fCtr_H_On'])+Delimiter+str(seq[StepNo]['fCtr_S_On'])+Delimiter+str(seq[StepNo]['fCtr_L_On'])
-    dataStr +=  Delimiter + str(seq[StepNo]['fCtr_H_Off'])+Delimiter+str(seq[StepNo]['fCtr_S_Off'])+Delimiter+str(seq[StepNo]['fCtr_L_Off'])
-    dataStr +=  Delimiter + str(seq[StepNo]['iTypeS'])
-    dataStr +=  Delimiter + str(seq[StepNo]['fStrb_A']) + Delimiter + str(seq[StepNo]['fStrb_B']) + Delimiter + str(seq[StepNo]['fStrb_C']) + Delimiter + str(seq[StepNo]['fStrb_D']) + Delimiter + str(seq[StepNo]['fStrb_E'])
-    dataStr +=  Delimiter + str(seq[StepNo]['lPattern'])
-    dataStr +=  Delimiter + str(seq[StepNo]['fStrb_H_On']) + Delimiter + str(seq[StepNo]['fStrb_S_On']) + Delimiter + str(seq[StepNo]['fStrb_L_On'])
-    dataStr +=  Delimiter + str(seq[StepNo]['fStrb_H_Off']) + Delimiter + str(seq[StepNo]['fStrb_S_Off']) + Delimiter + str(seq[StepNo]['fStrb_L_Off'])
-    dataStr +=  Delimiter + str(seq[StepNo]['lSeqTime'])
-    dataStr +=  ">"
+    dataStr += Delimiter + str(seq[StepNo]['fCtr_A'])
+    dataStr += Delimiter + str(seq[StepNo]['fCtr_B'])
+    dataStr += Delimiter + str(seq[StepNo]['fCtr_C'])
+    dataStr += Delimiter + str(seq[StepNo]['fCtr_D'])
+    dataStr += Delimiter + str(seq[StepNo]['fCtr_E'])
+    dataStr += Delimiter + str(seq[StepNo]['fCtr_H_On'])
+    dataStr += Delimiter + str(seq[StepNo]['fCtr_S_On'])
+    dataStr += Delimiter + str(seq[StepNo]['fCtr_L_On'])
+    dataStr += Delimiter + str(seq[StepNo]['fCtr_H_Off'])
+    dataStr += Delimiter + str(seq[StepNo]['fCtr_S_Off'])
+    dataStr += Delimiter + str(seq[StepNo]['fCtr_L_Off'])
+    dataStr += Delimiter + str(seq[StepNo]['iTypeS'])
+    dataStr += Delimiter + str(seq[StepNo]['fStrb_A'])
+    dataStr += Delimiter + str(seq[StepNo]['fStrb_B'])
+    dataStr += Delimiter + str(seq[StepNo]['fStrb_C'])
+    dataStr += Delimiter + str(seq[StepNo]['fStrb_D'])
+    dataStr += Delimiter + str(seq[StepNo]['fStrb_E'])
+    dataStr += Delimiter + str(seq[StepNo]['lPattern'])
+    dataStr += Delimiter + str(seq[StepNo]['fStrb_H_On'])
+    dataStr += Delimiter + str(seq[StepNo]['fStrb_S_On'])
+    dataStr += Delimiter + str(seq[StepNo]['fStrb_L_On'])
+    dataStr += Delimiter + str(seq[StepNo]['fStrb_H_Off'])
+    dataStr += Delimiter + str(seq[StepNo]['fStrb_S_Off'])
+    dataStr += Delimiter + str(seq[StepNo]['fStrb_L_Off'])
+    dataStr += Delimiter + str(seq[StepNo]['lSeqTime'])
+    dataStr += ">"
 
+    # Start event from Arduino
+    if out[0:4] == b'C000':
+        print('Sending step no.: ' + str(StepNo) + '\r\n')
+        print(dataStr + '\r\n')
+        ser.write(dataStr.encode('utf-8'))
+        StepNo += 1
 
-    #Start event from Arduino
-    if out[0:4] == b'C000' :
-            print('Sending step no.: ' + str(StepNo) + '\r\n')
-            print(dataStr + '\r\n')
-            ser.write(dataStr.encode('utf-8'))
-            StepNo += 1
-
-    #Collect next step
+    # Collect next step
     if StepNo >= len(seq):
         print('Last step has been reached. Exit')
         ser.close()
         exit()
-    #Exit event from Arduino
+
+    # Exit event from Arduino
     if out[0:4] == b'C999':
         print('Exit')
         ser.close()
